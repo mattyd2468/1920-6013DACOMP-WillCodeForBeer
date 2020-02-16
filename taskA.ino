2685b6ed36254c96d8c108770f541058198898cb
mattyd2468 <matty.dryhurst@hotmail.co.uk>
2020-02-16 16:39:57 +0000

    TASK A | Added value 0 check for 3 sensors including writing error to Serial

--- taskA.ino
+++ taskA.ino
@@ -1,29 +1,53 @@
-#include <Arduino.h>
-#include <pins_arduino.h>
+#include "Arduino.h"
+#include "Thermometer.h"
+#include "Humidity.h"
+#include "Air.h"
+#include "LED.h"
 
-enum State {
-  ON,
-  OFF
-};
+String errorMsg = "Error | There is an issue with the";
+int potPin = A0; // Potentiometer output connected to analog pin A0
+int potVal = 0; // Variable to store the input from the potentiometer
+Thermometer *thermometer = NULL;
 
-State LEDcurrent;
-const int LED_PIN = LED_BUILTIN;
+int humPin = A1; // Potentiometer output connected to analog pin A1
+int humVal = 0; // Variable to store the input from the potentiometer
+Humidity *humidity = NULL;
 
+int airPin = A2; // Potentiometer output connected to analog pin A2
+int airVal = 0; // Variable to store the input from the potentiometer
+Air *airSensor = NULL;
 
 void setup() {
-  LEDcurrent = OFF;
-  pinMode(LED_PIN, OUTPUT);
-  Serial.begin(115200);
-  Serial.println("Power on! ");
-
+	thermometer = new Thermometer(potPin);
+	humidity = new Humidity(humPin);
+	airSensor = new Air(airPin);
+	Serial.begin(115200);
+	Serial.println("Power on!");
 }
 
 void loop() {
-	if (LEDcurrent == OFF){
-		digitalWrite(LED_PIN, HIGH);
-		LEDcurrent = ON;
-		Serial.println("Power on! ");
+	//Check values are being received. Display suitable error
+	if (potVal == 0) {
+		Serial.println(errorMsg + "Thermometer");
 	}
+	if (humVal == 0) {
+		Serial.println(errorMsg + "Humidity Sensor");
+	}
+	if (airVal == 0){
+			Serial.println(errorMsg + "Air Sensor");
+		}
 
+	// thermometer sensor
+	potVal = thermometer->readSensor(potPin, potVal); // read the thermometer sensor value at the input pin
+	thermometer->setLEDColour(potVal, thermometer->led);
+
+	// humidity sensor
+	humVal = humidity->readSensor(humPin, humVal); // read the humidity sensor value at the input pin
+	humidity->setLEDColour(humVal, humidity->led);
+
+	// air sensor
+	airVal = airSensor->readSensor(airPin, airVal); // read the air sensor value at the input pin
+	airSensor->setLEDColour(potVal, airSensor->led);
+
+	delay(1000); // delay for 1 second
 }
-
