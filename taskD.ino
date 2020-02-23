dc5b9c10a4a0457fdbccfe629c70bec332f62a71
seanoconnormail <seanoconnormail@gmail.com>
2020-02-23 18:10:04 +0000

    TaskD | Rewritten TaskD and also made it compatible with the ESP32

--- taskD.ino
+++ taskD.ino
@@ -1,75 +1,69 @@
-//#include "Arduino.h"
+//#include <Arduino.h>
 //
-//enum MotionSensorState {
-//	OCCUPIED, VACANT
-//};
+//long unsigned int lowTime;
+//const long unsigned int pauseTime = 5000;	//TODO: 600,000 is 10 mins
 //
-//const int CHECK_DELAY = 10000; // TODO: 10 seconds for demo, 10 minutes for submission
-//const int LED_PIN = 13;
-//const int MOTION_SENSOR = 2; //TODO: Currently simulated using a button
-//MotionSensorState sensorCurrent;
+//boolean isDelayActive = true;	// Prevents duplicate output to console
+//boolean getLowTime;
 //
-//unsigned long int timeSinceOccupied;
-//int counter = 0;
-//bool occupied;
+//const int MOTION_SENSOR = 4;    	// PIR sensor pin
+//const int MOTION_INDICATOR = 12;	// TODO: LED pin, LED is not needed for task D!
+//const int calibrationTime = 30; 	// 30 seconds for initialising the PIR Sensor
 //
 //void setup() {
-//	pinMode(LED_PIN, OUTPUT);
-//	pinMode(MOTION_SENSOR, INPUT);
 //	Serial.begin(115200);
-//	sensorCurrent = VACANT;
-//	timeSinceOccupied = millis();
-//}
+//	// Setting pins and default values
+//	pinMode(MOTION_SENSOR, INPUT);
+//	pinMode(MOTION_INDICATOR, OUTPUT);
+//	digitalWrite(MOTION_SENSOR, LOW);
 //
-//boolean timeDiff(unsigned long start, int specifiedDelay) {
-//	return (millis() - start >= specifiedDelay);
-//}
-//
-///*
-// * TODO: LED is not needed for task D! This is only for demonstration purposes.
-// * 	OCCUPIED/VACANT should be output by Task C.
-// */
-//void ledOn() {
-//	digitalWrite(LED_PIN, HIGH);
-//	// Sets to VACANT after CHECK_DELAY
-//	if (timeDiff(timeSinceOccupied, CHECK_DELAY)) {
-//		sensorCurrent = VACANT;
+//	// Waiting for the PIR Sensor to initialise
+//	Serial.print("Initialising PIR Sensor ");
+//	for (int i = 0; i < calibrationTime; i++) {
+//		Serial.print(".");
+//		delay(1000);
 //	}
-//	return;
+//	Serial.println("|");
+//	Serial.println("SENSOR ACTIVE");
+//	delay(50);
 //}
 //
-//void ledOff() {
-//	digitalWrite(LED_PIN, LOW);
-//	sensorCurrent = VACANT;
-//	return;
+//void motionSensor() {
+//	if (digitalRead(MOTION_SENSOR) == HIGH) {
+//		digitalWrite(MOTION_INDICATOR, HIGH); // TODO: LED is on while motion is sensed
+//
+//		// Ensures we don't output OCCUPIED until the sensor has since been LOW
+//		if (isDelayActive) {
+//			isDelayActive = false;
+//			Serial.println("---");
+//			Serial.print("OCCUPIED at ");
+//			Serial.print(millis() / 1000);
+//			Serial.println(" sec");
+//			delay(50);
+//		}
+//		getLowTime = true;
+//	}
+//
+//	if (digitalRead(MOTION_SENSOR) == LOW) {
+//		digitalWrite(MOTION_INDICATOR, LOW); // LED is off when motion isn't sensed
+//
+//		// Getting the time where motion stopped being picked up.
+//		if (getLowTime) {
+//			lowTime = millis();
+//			getLowTime = false; // make sure this is only done at the start of a LOW phase
+//		}
+//		// if motion isn't picked up for more than the given pauseTime, the room is assumed to be VACANT.
+//		if (!isDelayActive && (millis() - lowTime) > pauseTime) {
+//			// Ensures we don't output VACANT until the sensor has since been HIGH
+//			isDelayActive = true;
+//			Serial.print("VACANT at ");
+//			Serial.print((millis()) / 1000);
+//			Serial.println(" sec");
+//			delay(50);
+//		}
+//	}
 //}
 //
-//void motionSensor(){
-//	MotionSensorState sensorOld = sensorCurrent;
-//
-//		if (digitalRead(MOTION_SENSOR) == 1) {
-//			sensorCurrent = OCCUPIED;
-//		}
-//
-//		switch (sensorCurrent) {
-//		case OCCUPIED:
-//			ledOn();
-//			break;
-//
-//		case VACANT:
-//			ledOff();
-//			break;
-//		}
-//
-//		if (sensorOld != sensorCurrent) {
-//			Serial.println("state change");
-//			Serial.println(sensorCurrent);
-//			timeSinceOccupied = millis();
-//		}
-//}
-//
-//// The loop function is called in an endless loop
 //void loop() {
 //	motionSensor();
 //}
-//
