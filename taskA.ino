3772bca42fa4b865259b3a944574500c45abe79d
mattyd2468 <matty.dryhurst@hotmail.co.uk>
2020-02-25 14:46:47 +0000

    TASK A | Put Task A, B and D together

--- taskA.ino
+++ taskA.ino
@@ -6,87 +6,89 @@
 
 #define DHTPIN 4 // the pin value for the DHT11 sensor
 #define DHTTYPE DHT11
-
-enum MotionSensorState {
-	OCCUPIED, VACANT
-};
-
 bool firstLoop = true; //Variable to store if it is the first loop or not
 int potVal = 0; // Variable to store temperature value
 int humVal = 0; // Variable to store the humidity value
-const int CHECK_DELAY = 10000; // TODO: 10 seconds for demo, 10 minutes for submission
-const int LED_PIN = 13;
-const int MOTION_SENSOR = 2; //TODO: Currently simulated using a button
-MotionSensorState sensorCurrent;
 
-unsigned long int timeSinceOccupied;
-int counter = 0;
-bool occupied;
+//taskD variables
+long unsigned int lowTime;
+const long unsigned int pauseTime = 5000;	//TODO: 600,000 is 10 mins
+
+boolean isDelayActive = true;	// Prevents duplicate output to console
+boolean getLowTime;
+
+const int MOTION_SENSOR = 18;    	// PIR sensor pin
+const int MOTION_INDICATOR = 12;	// TODO: LED pin, LED is not needed for task D!
+const int calibrationTime = 30; 	// 30 seconds for initialising the PIR Sensor
 
 Thermometer *thermometer = NULL;
 Humidity *humidity = NULL;
 DHTesp dht; // object to store the DHT11 sensor
 TempAndHumidity tempHum; // the object to store the temperature and humidity values
+LED *led = new LED(23, 22, 21); // the LED which shows the status of the sensors, pins 23,22,21
+
+TemperatureStatus tempStatus; // object to store the temperature status
+HumidityStatus humStatus; // object to store the humidity status
+
+//taskD method
+void motionSensor() {
+	if (digitalRead(MOTION_SENSOR) == HIGH) {
+		digitalWrite(MOTION_INDICATOR, HIGH); // TODO: LED is on while motion is sensed
+
+		// Ensures we don't output OCCUPIED until the sensor has since been LOW
+		if (isDelayActive) {
+			isDelayActive = false;
+			Serial.println("---");
+			Serial.print("OCCUPIED at ");
+			Serial.print(millis() / 1000);
+			Serial.println(" sec");
+			delay(50);
+		}
+		getLowTime = true;
+	}
+
+	if (digitalRead(MOTION_SENSOR) == LOW) {
+		digitalWrite(MOTION_INDICATOR, LOW); // LED is off when motion isn't sensed
+
+		// Getting the time where motion stopped being picked up.
+		if (getLowTime) {
+			lowTime = millis();
+			getLowTime = false; // make sure this is only done at the start of a LOW phase
+		}
+		// if motion isn't picked up for more than the given pauseTime, the room is assumed to be VACANT.
+		if (!isDelayActive && (millis() - lowTime) > pauseTime) {
+			// Ensures we don't output VACANT until the sensor has since been HIGH
+			isDelayActive = true;
+			Serial.print("VACANT at ");
+			Serial.print((millis()) / 1000);
+			Serial.println(" sec");
+			delay(50);
+		}
+	}
+}
 
 void setup() {
-	thermometer = new Thermometer(DHTPIN);
-	humidity = new Humidity(DHTPIN);
+	thermometer = new Thermometer(DHTPIN, led);
+	humidity = new Humidity(DHTPIN, led);
 
 	Serial.begin(115200); // @suppress("Ambiguous problem")
 	dht.setup(DHTPIN, DHTesp::DHT11); // set up the DHT11 sensor
-	//TASK D setup
-	pinMode(LED_PIN, OUTPUT);
-	pinMode(MOTION_SENSOR, INPUT);
-	sensorCurrent = VACANT;
-	timeSinceOccupied = millis();
-}
 
-boolean timeDiff(unsigned long start, int specifiedDelay) {
-	return (millis() - start >= specifiedDelay);
-}
+	//taskD setup
+		// Setting pins and default values
+		pinMode(MOTION_SENSOR, INPUT);
+		pinMode(MOTION_INDICATOR, OUTPUT);
+		digitalWrite(MOTION_SENSOR, LOW);
 
-//Below is for TASK D. Can this be put into classes?
-/*
- * TODO: LED is not needed for task D! This is only for demonstration purposes.
- * 	OCCUPIED/VACANT should be output by Task C.
- */
-void ledOn() {
-	digitalWrite(LED_PIN, HIGH);
-	// Sets to VACANT after CHECK_DELAY
-	if (timeDiff(timeSinceOccupied, CHECK_DELAY)) {
-		sensorCurrent = VACANT;
-	}
-	return;
-}
-
-void ledOff() {
-	digitalWrite(LED_PIN, LOW);
-	sensorCurrent = VACANT;
-	return;
-}
-
-void motionSensor() {
-	MotionSensorState sensorOld = sensorCurrent;
-
-	if (digitalRead(MOTION_SENSOR) == 1) {
-		sensorCurrent = OCCUPIED;
-	}
-
-	switch (sensorCurrent) {
-	case OCCUPIED:
-		ledOn();
-		break;
-
-	case VACANT:
-		ledOff();
-		break;
-	}
-
-	if (sensorOld != sensorCurrent) {
-		Serial.println("state change");
-		Serial.println(sensorCurrent);
-		timeSinceOccupied = millis();
-	}
+		// Waiting for the PIR Sensor to initialise
+		Serial.print("Initialising PIR Sensor ");
+		for (int i = 0; i < calibrationTime; i++) {
+			Serial.print(".");
+			delay(1000);
+		}
+		Serial.println("|");
+		Serial.println("SENSOR ACTIVE");
+		delay(50);
 }
 
 void loop() {
@@ -108,12 +110,11 @@ void loop() {
 		tempHum = dht.getTempAndHumidity();
 
 		// set LED colours for temp and humidity sensor
-		thermometer->setLEDColour(tempHum.temperature, thermometer->led);
-		humidity->setLEDColour(tempHum.humidity, humidity->led);
+		tempStatus = thermometer->calculateStatus(tempHum.temperature, led);
+		humStatus = humidity->calculateStatus(tempHum.humidity, led);
 
-		//Call motion sensor method (TASKD)
-		motionSensor();
+		led->setLEDColour(tempStatus, humStatus);
 		delay(2000); // delay for 2 seconds - this is needed for the DHT11 sensor as a minimum
+		motionSensor(); //Call taskD code
 	}
 }
-
