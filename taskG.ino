e145e942f85521430f1fd84a986743202aa728d1
mattyd2468 <matty.dryhurst@hotmail.co.uk>
2020-03-03 12:08:07 +0000

    TASKG| Added some criteria for buzzer to sound

--- taskG.ino
+++ taskG.ino
@@ -9,7 +9,7 @@
 #define DHTTYPE DHT11
 const int BUZZER = 25; //Buzzer pin
 const int MOTION_SENSOR = 18;    	// PIR sensor pin
-const int MOTION_INDICATOR = 12;// TODO: LED pin, LED is not needed for task D! Change to buzzer maybe?
+const int MOTION_INDICATOR = 12; // TODO: LED pin, LED is not needed for task D! Change to buzzer maybe?
 LED *led = new LED(23, 22, 21); // the LED which shows the status of the sensors, pins 23,22,21
 
 //Variable initialisation
@@ -17,14 +17,15 @@ bool firstLoop = true; //Variable to store if it is the first loop or not
 int potVal = 0; // Variable to store temperature value
 int humVal = 0; // Variable to store the humidity value
 long unsigned int lowTime; //Variable for PIR sensor low time
-const long unsigned int pauseTime = 5000;//TODO: 600,000 is 10 mins currently 5 secs
+const long unsigned int pauseTime = 5000; //TODO: 600,000 is 10 mins currently 5 secs
 boolean isDelayActive = true;	// Prevents duplicate output to console
 boolean getLowTime;
 const int calibrationTime = 30; // 30 seconds for initialising the PIR Sensor
-int freq = 2000; //buzzer
-int channel = 0; //buzzer
-int resolution = 8; //buzzer
-int dutyCycle = 128;
+int freq = 2000; //buzzer set up. Doesnt need to change
+int channel = 0; //buzzer set up. Doesnt need to change
+int resolution = 8; //buzzer set up. Doesnt need to change
+int dutyCycle = 128; //buzzer 128 means noise. Set to 0 for silence
+enum LEDColour colour; //Used for current LED colour
 
 //Setting up the objects
 Thermometer *thermometer = NULL;
@@ -68,7 +69,8 @@ void motionSensor() {
 
 		// Ensures we don't output OCCUPIED until the sensor has since been LOW
 		if (isDelayActive) {
-			buzzFor1Second();
+			colour = led->currentColour;
+			buzz(colour);
 			isDelayActive = false;
 			Serial.println("---");
 			Serial.print("OCCUPIED at ");
@@ -107,11 +109,11 @@ void powerOnTest() {
 	float hum = tempHum.humidity;
 
 	//DHT11 maximum values. Need to add nan check
-	 if (temp < 0  || temp > 50) {
-	 Serial.println("Temperature Error");
-	 }
-	 if (hum < 20  || hum > 80) {
-		 Serial.println("Humidity Error");
+	if (temp < 0 || temp > 50) {
+		Serial.println("Temperature Error");
+	}
+	if (hum < 20 || hum > 80) {
+		Serial.println("Humidity Error");
 	}
 
 	firstLoop = false;
@@ -127,10 +129,19 @@ void tempAndHumSensor() {
 	delay(2000); // delay for 2 seconds - this is needed for the DHT11 sensor as a minimum
 }
 
-void buzzFor1Second() {
-	ledcWrite(channel, dutyCycle);
-	delay(1000);
-	ledcWrite(channel, 0);
+void buzz(LEDColour colour) {
+	//if led red buzz for 3 sec
+	if (colour == LEDColour::RED) {
+		ledcWrite(channel, dutyCycle);
+		delay(3000);
+		ledcWrite(channel, 0);
+	}
+	//else buzz for 1 secs
+	else {
+		ledcWrite(channel, dutyCycle);
+		delay(1000);
+		ledcWrite(channel, 0);
+	}
 }
 
 void loop() {
