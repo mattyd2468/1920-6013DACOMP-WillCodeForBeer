7efd4c5b408eda94d1a912c77ef720eaa87ae77d
mattyd2468 <matty.dryhurst@hotmail.co.uk>
2020-03-12 18:59:55 +0000

    V2 | Bugfix on enum class

--- HumidityStatus.h
+++ HumidityStatus.h
@@ -1,3 +1,8 @@
+#ifndef HUMIDITYSTATUS_H_
+#define HUMIDITYSTATUS_H_
+
 enum class HumidityStatus{
 	GREEN, RED, AMBER
 };
+
+#endif
--- TemperatureStatus.h
+++ TemperatureStatus.h
@@ -1,3 +1,8 @@
+#ifndef TEMPERATURESTATUS_H_
+#define TEMPERATURESTATUS_H_
+
 enum class TemperatureStatus{
 	GREEN, AMBER, RED
 };
+
+#endif
